abfbb2809dc17171928f0b1013f13dca09b15abc
NoahjmHolt <noahjmholt@gmail.com>
2026-03-23 17:29:09 -0600

    clock start
    created elements neccesary for a clock to be in use but not quit at that point yet.

--- main.py
+++ main.py
@@ -11,6 +11,10 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    # clock
+    clock = pygame.time.Clock()
+    dt = 0
+
     # keep playing while game is open
     while True:
         log_state()
@@ -25,6 +29,10 @@ def main():
         # make screen filled and then display so user knows where ship and rocks are
         screen.fill('black')
         pygame.display.flip()
+
+        # time keeps ticking
+        new_dt = clock.tick(60)
+        dt = new_dt / 1000
 
 if __name__ == "__main__":
     main()
